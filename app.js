7dd1116a68702e9d4d666f41090b1253cdaa8d52
Jhonny <gonzodzen@mail.ru>
2022-11-13 13:13:32 +0300

    Refactoring to read file.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const file = path.join(__dirname, 'b.js')
-const stream = fs.ReadStream(file, {encoding: 'utf-8'})
-stream.on('readable', ()=>{
+const stream = fs.createReadStream(file, {encoding: 'utf-8'})
+stream.on('readable', (err)=>{
+    if(err){
+        throw Error
+    }
     let data = stream.read()
     if(data!==null){
         console.log(data)
     }
 })
 stream.on('end', (err)=>{
-   
+   if(err){
+    throw Error
+   }
 })
