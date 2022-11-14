44d7459ea6f14496d1b40d0988f715cb02e035bd
Jhonny <gonzodzen@mail.ru>
2022-11-14 19:44:10 +0300

    Writable stream methods: cork and uncork.

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const fs = require('fs')
 
-const rs = fs.createReadStream('./package-lock.json')
-const ws = fs.createWriteStream('newFile.txt')
+const wr = fs.createWriteStream('./buffer.js')
 
-ws.on('close', ()=>{
-    console.log('Writable stream has been closed!')
-})
-ws.on('error', (err)=>{
-    console.log(`Error occured: ${err}`)
-})
-ws.on('finish', ()=>{
-    console.log('Writable stream has been finished!')
-})
-ws.on('pipe', ()=>{
-    console.log('Piped to Readable stream!')
-})
-ws.on('unpipe', ()=>{
-    console.log('Unpiped to Readable stream!')
-})
+wr.cork()
 
-rs.pipe(ws)
+wr.write('1\n')
+wr.write('2\n')
+wr.write('3\n')
+wr.write('4\n')
+
+wr.uncork()
