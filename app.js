43ea7015bf5d1ef94b11ef0f76f41aa3140277e4
Jhonny <gonzodzen@mail.ru>
2022-11-29 18:51:13 +0300

    Sync create folder.

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
 
-const start = Date.now()
-
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('1 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('2 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('3 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('4 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('5 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('6 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('7 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('8 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('9 end', Date.now() - start)
-})
-crypto.pbkdf2('123123ppp', '5', 100000, 64, 'sha512', ()=>{
-  console.log('10 end', Date.now() - start)
-})
+ fs.mkdirSync(path.resolve(__dirname, 'dir'))
