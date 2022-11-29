8350f843a450b1f4526ca1472e3dfa00b89c75cd
Jhonny <gonzodzen@mail.ru>
2022-11-29 20:35:28 +0300

    Cluster and whorker.

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
+const os = require("os");
+const cluster = require("cluster");
 
-const fs = require('fs')
-const path = require('path')
-
-
-const writeFileAsync = (path, data)=>{
-  return new Promise((res, rej)=> fs.writeFile(path, data, (err)=>{
-    if(err) rej(err.message)
-    res()
-  }))
+if (cluster.isMaster) {
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork()
+  }
 }
-const appendFileAsync = (path, data)=>{
-  return new Promise((res, rej)=> fs.appendFile(path, data, (err)=>{
-    if(err) rej(err.message)
-    res()
-  }))
+else{
+  console.log(`Workher ${process.pid} is gob`)
+  setInterval(()=>{
+    console.log(`${process.pid} is gob`)
+  }, 5000)
 }
-const readFileAsync = async (path)=>{
-  return new Promise((res, rej)=> fs.readFile(path, {encoding: 'utf-8'}, (err, data)=>{
-    if(err) rej(err.message)
-    res(data)
-  }))
-}
-const rmFileAsync = async (path)=>{
-  return new Promise((res, rej)=> fs.rm(path, {encoding: 'utf-8'}, (err)=>{
-    if(err) rej(err.message)
-    res()
-  }))
-}
-
-writeFileAsync(path.resolve(__dirname, 'text.txt'), 'data')
-.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '123'))
-.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '456'))
-.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '789'))
-.then(readFileAsync(path.resolve(__dirname, 'text.txt')))
-.then(data => console.log(data))
-//.then(rmFileAsync(path.resolve(__dirname, 'text.txt')))
-.catch(err => console.log(err))
