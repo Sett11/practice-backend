56eb3686578c46b66db3cc575a81949025c54242
Jhonny <gonzodzen@mail.ru>
2022-11-29 20:44:35 +0300

    subscribe to event

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ if (cluster.isMaster) {
   for (let i = 0; i < os.cpus().length; i++) {
     cluster.fork()
   }
+  cluster.on('exit', (worker)=>{
+    console.log(`Worker c id: ${worker.process.pid} is dead`)
+  })
 }
 else{
   console.log(`Workher ${process.pid} is gob`)
