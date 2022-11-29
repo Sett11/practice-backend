8835d19ad5cb24aa9dbac92508e3f7122f5fd333
Jhonny <gonzodzen@mail.ru>
2022-11-29 19:14:44 +0300

    Async write file.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.writeFile(path.resolve(__dirname, 'text.txt'), 'Fack', (err)=>{
+
+fs.appendFile(path.resolve(__dirname, 'text.txt'), 'Fack', (err)=>{
   if(err){
     throw new Error
   }
