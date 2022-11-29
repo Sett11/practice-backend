886115e9589fe974ff48b83a2cb08b20adc1b6d5
Jhonny <gonzodzen@mail.ru>
2022-11-29 18:55:16 +0300

    Sync create recursive folders.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 const path = require('path')
 
- fs.mkdirSync(path.resolve(__dirname, 'dir'))
+ fs.mkdirSync(path.resolve(__dirname, 'dir', 'dir1', 'dir2'), {recursive: true})
