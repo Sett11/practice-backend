a9614e3d91e2563a527a3f2befc875fc08ca9daf
Jhonny <gonzodzen@mail.ru>
2022-11-29 21:57:26 +0300

    WriteStream.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const fs = require('fs')
 const path = require('path')
 
 
-const pathToFile = path.resolve(__dirname, 'file.txt')
+const pathToFile = path.resolve(__dirname, 'file2.txt')
 
-const stream = fs.createReadStream(pathToFile)
-stream.on('data', (chunk)=>{
-  console.log(chunk)
-})
+const stream = fs.createWriteStream(pathToFile)
+
+for(let i = 0; i < 100; i++){
+  stream.write(i + '\n')
+}
+stream.end()
