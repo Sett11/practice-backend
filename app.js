55b96e32cc06a88b592ce110847261e8840970f2
Jhonny <gonzodzen@mail.ru>
2022-11-29 21:47:31 +0300

    ReadStream.

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
-const text = `ahhhahahahha
-100
-ahhhahaha
-100
-ahhahahaha
-`
+
 const pathToFile = path.resolve(__dirname, 'file.txt')
 
-fs.writeFile(pathToFile, text, (err)=>{
-  if(err) throw new Error
+const stream = fs.createReadStream(pathToFile)
+stream.on('data', (chunk)=>{
+  console.log(chunk)
 })
-
-let counter = 100
-
-while(counter){
-  fs.appendFile(pathToFile, text, (err)=>{
-  if(err) throw new Error
-})
-counter--
-} 
