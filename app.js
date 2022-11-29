627f7e0d1eff774dd378ea50bdc2af4063e04ea1
Jhonny <gonzodzen@mail.ru>
2022-11-29 19:30:38 +0300

    Write file with promises.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+
 const fs = require('fs')
 const path = require('path')
 
 
-fs.appendFile(path.resolve(__dirname, 'text.txt'), 'Fack', (err)=>{
-  if(err){
-    throw new Error
-  }
-})
+const writeFileAsync = (path, data)=>{
+  return new Promise((res, rej)=> fs.writeFile(path, data, (err)=>{
+    if(err) rej(err.message)
+    res()
+  }))
+}
+const appendFileAsync = (path, data)=>{
+  return new Promise((res, rej)=> fs.appendFile(path, data, (err)=>{
+    if(err) rej(err.message)
+    res()
+  }))
+}
+
+writeFileAsync(path.resolve(__dirname, 'text.txt'), 'data')
+.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '123'))
+.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '456'))
+.then(appendFileAsync(path.resolve(__dirname, 'text.txt'), '789'))
+.catch(err=> console.log(err))
