fbd20057108f917d12a9d20100622c84a4fea7b6
Jhonny <gonzodzen@mail.ru>
2022-11-29 19:07:24 +0300

    Async create and write file.

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-console.log('Start')
-
- fs.rmdir(path.resolve(__dirname, 'dir'), (err)=>{
-  if(err) console.error(err)
-  console.log('Folder delete')
- })
-
- console.log('End')
+fs.writeFile(path.resolve(__dirname, 'text.txt'), 'Fack', (err)=>{
+  if(err){
+    throw new Error
+  }
+})
